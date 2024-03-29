fb845719bc20b92d26011dbe00757e5594ba59aa
umyedi <timoleroux2006@gmail.com>
2024-03-29 22:58:35 +0100

    Ajout correction questions + tests
    Ajout corrections questions : 1, 2, 3.3, 10, 17, 20, 21, 23,

--- tests.py
+++ tests.py
@@ -1,67 +1,158 @@
 import utils
+from random import randint
+
 
 def test_verif_etage_critique(function: callable):
-    raise ValueError("The test function isn't done yet.")
+
+    utils._set_values(etages=10, critique=2, oeufs=float("inf"))
+    assert function(utils._critique) == True
+
+    utils._set_values(critique=randint(3, 100))
+    assert function(utils._critique - 1) == False
+    assert function(utils._critique) == True
+    assert function(utils._critique + 1) == False
+
 
 def test_critique_un_seul_oeuf(function: callable):
-    raise ValueError("The test function isn't done yet.")
+    utils._set_values(etages=10, critique=1, oeufs=1)
+    assert function(utils._etages) == 1
+
+    utils._set_values(etages=100, critique=randint(2, 100), oeufs=1)
+    assert function(utils._etages) == utils._critique
+
+    utils._set_values(etages=10, critique=100, oeufs=1)
+    assert function(utils._etages) == 11
+
 
 def test_critique_dichotomie(function: callable):
-    raise ValueError("The test function isn't done yet.")
-    
+    utils._set_values(etages=1, critique=1, oeufs=1)
+    assert function(utils._etages) == 1
+
+    utils._set_values(etages=100, critique=randint(2, 100), oeufs=100)
+    assert function(utils._etages) == utils._critique
+
+    utils._set_values(etages=10, critique=100, oeufs=10)
+    assert function(utils._etages) == 11
+
+
 def test_L(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_Lprec(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_strategie(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_faces(function: callable):
-    raise ValueError("The test function isn't done yet.")
-    
+    assert function((0, 0, 0)) == [
+        [(0, 0, 0), (0, 1, 0), (1, 0, 0), (1, 1, 0)],
+        [(0, 0, 1), (0, 1, 1), (1, 0, 1), (1, 1, 1)],
+        [(0, 0, 0), (0, 0, 1), (1, 0, 0), (1, 0, 1)],
+        [(0, 1, 0), (0, 1, 1), (1, 1, 0), (1, 1, 1)],
+        [(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1)],
+        [(1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1)],
+    ]
+    assert function((4, -5, 1)) == [
+        [(4, -5, 1), (4, -4, 1), (5, -5, 1), (5, -4, 1)],
+        [(4, -5, 2), (4, -4, 2), (5, -5, 2), (5, -4, 2)],
+        [(4, -5, 1), (4, -5, 2), (5, -5, 1), (5, -5, 2)],
+        [(4, -4, 1), (4, -4, 2), (5, -4, 1), (5, -4, 2)],
+        [(4, -5, 1), (4, -5, 2), (4, -4, 1), (4, -4, 2)],
+        [(5, -5, 1), (5, -5, 2), (5, -4, 1), (5, -4, 2)],
+    ]
+
+
 def test_nb_faces_visibles(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_surface_externe(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_voisins(function: callable):
-    raise ValueError("The test function isn't done yet.")
-    
+    x, y, z = randint(-100, 100), randint(-100, 100), randint(-100, 100)
+    assert len(function((x, y, z))) == 26
+    assert (x, y, z) not in function((x, y, z))
+    assert function((x, y, z)) == [
+        (x - 1, y - 1, z - 1),
+        (x - 1, y - 1, z),
+        (x - 1, y - 1, z + 1),
+        (x - 1, y, z - 1),
+        (x - 1, y, z),
+        (x - 1, y, z + 1),
+        (x - 1, y + 1, z - 1),
+        (x - 1, y + 1, z),
+        (x - 1, y + 1, z + 1),
+        (x, y - 1, z - 1),
+        (x, y - 1, z),
+        (x, y - 1, z + 1),
+        (x, y, z - 1),
+        (x, y, z + 1),
+        (x, y + 1, z - 1),
+        (x, y + 1, z),
+        (x, y + 1, z + 1),
+        (x + 1, y - 1, z - 1),
+        (x + 1, y - 1, z),
+        (x + 1, y - 1, z + 1),
+        (x + 1, y, z - 1),
+        (x + 1, y, z),
+        (x + 1, y, z + 1),
+        (x + 1, y + 1, z - 1),
+        (x + 1, y + 1, z),
+        (x + 1, y + 1, z + 1),
+    ]
+
+
 def test_evolution(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_faces_adjacentes_sommet(function: callable):
-    raise ValueError("The test function isn't done yet.")
-    
+    F = [[0, 1, 2, 3], [0, 1, 4], [1, 2, 4], [2, 3, 4], [3, 0, 4]]
+    assert function(F, 0) == [[0, 1, 2, 3], [0, 1, 4], [3, 0, 4]]
+
+
 def test_faces_adjacentes_cote(function: callable):
+    F = [[0, 1, 2, 3], [0, 1, 4], [1, 2, 4], [2, 3, 4], [3, 0, 4]]
+    assert function(F, 0) == [[0, 1, 4], [1, 2, 4], [2, 3, 4], [3, 0, 4]]
+
+
+def test_triangule(function: callable):  # TODO
     raise ValueError("The test function isn't done yet.")
-    
-def test_triangule(function: callable):
-    raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_variables(b, f):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_valide_sommet(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_valide_face(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_valide_darete(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_faces_voisines(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_composante_connexe(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_separe_face(function: callable):
     raise ValueError("The test function isn't done yet.")
-    
+
+
 def test_serialise(function: callable):
     raise ValueError("The test function isn't done yet.")
--- utils.py
+++ utils.py
@@ -1,26 +1,51 @@
-_nombre_etages = 10
-_etage_critique = 5
-_nombre_oeufs = 10
+_etages = 10  # Nombre d'étages de l'immeuble
+_critique = 5  # Plus petit étage pour lequel l'oeufs se casse
+_oeufs = 10  # Nombre d'oeufs disponibles
+
+
+def _set_values(etages=None, critique=None, oeufs=None):
+    global _etages, _critique, _oeufs
+    if etages:
+        _etages = etages
+    if critique:
+        _critique = critique
+    if oeufs:
+        _oeufs = oeufs
+
 
 Infty = float("inf")
 
+
 def lacher(n):
+    global _oeufs, _critique
+
     if not isinstance(n, int):
         raise ValueError("n must be of type 'int'.")
     if n <= 0:
         raise ValueError("n must be greater than 0")
-    return n >= _etage_critique
+
+    if n >= _critique:
+        if _oeufs == 0:
+            raise ValueError("Vous n'avez plus d'oeufs à lancer")
+
+        _oeufs -= 1
+        return True
+
+    return False
+
 
 class Sommet:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-        self.darete = None # à remplir plus tard
+        self.darete = None  # à remplir plus tard
+
 
 class Face:
     def __init__(self):
-        self.darete = None # à remplir plus tard
+        self.darete = None  # à remplir plus tard
+
 
 class DArete:
     def __init__(self, source, but, face):
@@ -35,7 +60,8 @@ class DArete:
         self.source = source
         self.but = but
         self.face = face
-        self.suivant = None # à remplir plus tard
-        self.miroir = None # à remplir plus tard
+        self.suivant = None  # à remplir plus tard
+        self.miroir = None  # à remplir plus tard
 
-def sql_execute(request): ... # TODO
+
+def sql_execute(request): ...  # TODO
